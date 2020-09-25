8c4e1504ac6e7e0592d9b00c4eafd94c03d46f82
Luiz Fernando <luizepauloxd@gmail.com>
2020-09-25 19:38:59 -0300

    feat: adds root import plugin to base config files

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,5 @@ import { module } from 'config/webpack.config.prod';
 
 module.exports = {
   presets: ['babel-preset-react-app'],
+  plugins: [['babel-plugin-root-import', { rootPathSuffix: 'src' }]],
 };
