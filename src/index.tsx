0d41b777431650c3e21e59b3f1ca63519f68cead
Luiz Fernando <luizepauloxd@gmail.com>
2020-12-28 01:55:36 -0300

    feat: adds apollo configuration

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,20 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import Router from '~/router';
+import { client } from '~/config';
 import { Theme, GlobalStyle } from '~/design';
 import serviceWorker from '~/config/serviceWorker';
+import { ApolloProvider } from '@apollo/client';
 
 ReactDOM.render(
-  <ThemeProvider theme={Theme}>
-    <div>
-      <GlobalStyle />
-      <Router />
-    </div>
-  </ThemeProvider>,
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={Theme}>
+      <div>
+        <GlobalStyle />
+        <Router />
+      </div>
+    </ThemeProvider>
+  </ApolloProvider>,
   document.getElementById('root'),
 );
 
