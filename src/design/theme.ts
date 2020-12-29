040bfaf49b064726e4c70a0f2cbf59ab9887400e
Luiz Fernando <luizepauloxd@gmail.com>
2020-12-29 08:36:53 -0300

    feat: adds theme.ts to the twitter react code

--- src/design/theme.ts
+++ src/design/theme.ts
@@ -1,6 +1,53 @@
+import { transparentize } from 'polished';
+
+const makeTransparent = transparentize(50);
+
+const blue = '#1E96EB';
+const yellow = '#FEA432';
+const purple = '#6D42B8';
+const green = '#1EB65F';
+const orange = '#F15429';
+const pink = '#DA2454';
+const black = '#000';
+const bluishGrey = '#151D26';
+const white = '#FFF';
+const lightenGrey = '#7D8E9B';
+const grey = '#576A7B';
+const whiterGrey = '#e8ebee';
+
+const accents = {
+  blue,
+  transparentBlue: makeTransparent(blue),
+  yellow,
+  transparentYellow: makeTransparent(yellow),
+  pink,
+  transparentPink: makeTransparent(pink),
+  purple,
+  transparentPurple: makeTransparent(purple),
+  orange,
+  transparentOrange: makeTransparent(orange),
+  green,
+  transparentGreen: makeTransparent(green),
+};
+
+export const DarkTheme = {
+  colors: {
+    backgroundColor: bluishGrey,
+    textColor: white,
+    greyedText: lightenGrey,
+    selectionColor: white,
+    greySearchInput: lightenGrey,
+    accents,
+  },
+};
+
 export const Theme = {
   colors: {
-    backgroundColor: '#FFF',
-    textColor: '#000',
+    backgroundColor: white,
+    textColor: black,
+    greyedText: grey,
+    selectionColor: black,
+    greyTextInput: whiterGrey,
+    accents,
   },
 };
